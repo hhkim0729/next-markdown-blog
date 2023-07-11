a225c85548a1de82c39b0222f9b0ebcdc95881f3
hhkim0729 <hhkim0729@gmail.com>
2023-07-11 18:25:16 +0900

    Style post list

--- components/PostPreviewList.tsx
+++ components/PostPreviewList.tsx
@@ -7,16 +7,23 @@ type Props = {
 
 export default function PostPreviewList({ posts }: Props) {
   return (
-    <div>
+    <div className="mt-4">
       {posts.map((post, i) => (
-        <Link key={i} href={`/${post.slug}`}>
-          <h2>{post.title}</h2>
+        <Link
+          key={i}
+          href={`/${post.slug}`}
+          className="block border-b border-gray-300 p-2 hover:bg-gray-100"
+        >
+          <h2 className="text-2xl font-medium">{post.title}</h2>
           <p>{post.date}</p>
-          <p>{post.description}</p>
+          <p className="text-sm">{post.description}</p>
           {post?.tags && (
             <div>
               {post.tags.map((tag) => (
-                <span key={tag}>{`#${tag}`}</span>
+                <span
+                  key={tag}
+                  className="text-sm mr-2 text-gray-500"
+                >{`#${tag}`}</span>
               ))}
             </div>
           )}
